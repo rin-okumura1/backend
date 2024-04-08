e30bff8bb4f5f9f3ea9a7e1991edd0eb6698913c
rin-okumura1 <davidnobati@gmail.com>
2024-04-08 18:33:37 -0300

    asd

--- src/routes/Router/companyRouter.js
+++ src/routes/Router/companyRouter.js
@@ -13,13 +13,20 @@ companyRouter.get("/:id",async (req,res)=>{
     res.json(await Compania.findByPk(id))
 })
 
+async function existe(nombre){
+    let retorno =await Compania.findAll({where:nombre})
+    return retorno == undefined
+}
 
 companyRouter.post("/",async (req,res)=>{
     
-    let retorno =await Compania.create(req.body)
+    let retorno=""
+if(existe(req.body.nombre)==false) {retorno=await Compania.create(req.body);
+
     console.log(retorno)
         res.json(retorno).status(201)    
-   
+}
+else res.json({message:"valide que el nombre no este repetido"})
     
 })
 
