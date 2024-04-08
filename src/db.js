c079180932cd3b2f78e0751d4d46ebeb8cc83ed9
rin-okumura1 <davidnobati@gmail.com>
2024-04-08 11:54:46 -0300

    sube

--- src/db.js
+++ src/db.js
@@ -11,8 +11,12 @@ const { DATABASE_URL } = process.env;
 
 // console.log(pool)
 
-const sequelize = new Sequelize(
-    DATABASE_URL)
+const sequelize = new Sequelize("postgresql://postgres:YNUAvYlEXTjCoyOoPcjluBehIzjNIUZD@postgres.railway.internal:5432/railway",
+
+{
+  logging: false, // set to console.log to see the raw SQL queries
+  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+})
 
 UserModel(sequelize);
 TicketModel(sequelize);
