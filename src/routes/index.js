a8d903ec5fcf6ce4a48ca4e22763a1619132b35c
rin-okumura1 <davidnobati@gmail.com>
2024-04-08 22:06:53 -0300

    asd

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,8 @@ const { Router }= require("express")
 const usersRouter = require("./Router/usersRouter")
 const ticketsRouter = require("./Router/ticketsRouter")
 const companyRouter = require("./Router/companyRouter")
+const recordRouter=require('./Router/recordRouter')
+
 
 const mainRouter = Router();
 
