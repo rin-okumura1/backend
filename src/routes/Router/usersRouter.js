b01e181b12f8075e6591257f9b78d97248553798
rin-okumura1 <davidnobati@gmail.com>
2024-04-09 10:47:17 -0300

    Nan

--- src/routes/Router/usersRouter.js
+++ src/routes/Router/usersRouter.js
@@ -11,7 +11,7 @@ usersRouter.get("/",async (req,res)=>{
 usersRouter.get("/:id",async (req,res)=>{
     let id = parseInt(req.params.id);
     console.log(id)
-    if(typeof id !="number")res.json("jodete perra")
+    if(id ==NaN)res.json("jodete perra")
     else{
         let user=await Usuario.findByPk(id)
         res.json(user)
