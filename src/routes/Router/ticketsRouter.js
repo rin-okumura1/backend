8f618bcacfc692d3424d8dfdbc6d06fcbb4faa3e
rin-okumura1 <davidnobati@gmail.com>
2024-04-08 22:48:52 -0300

    post ticket

--- src/routes/Router/ticketsRouter.js
+++ src/routes/Router/ticketsRouter.js
@@ -1,21 +1,21 @@
 const {Router} = require("express");
 const ticketRouter = Router()
-const {ticket}=require('../../db')
+const {Ticket}=require('../../db')
 
 
 ticketRouter.get("/",async (req,res)=>{
-    res.json(await ticket.findAll())
+    res.json(await Ticket.findAll())
 })
 
 ticketRouter.get("/:id",async (req,res)=>{
     const { id } = req.params;
     console.log(id)
-    res.json(await ticket.findByPk(id))
+    res.json(await Ticket.findByPk(id))
 })
 
 
 ticketRouter.post("/",async (req,res)=>{
-    await ticket.create({
+    await Ticket.create({
         estado: req.body.estado,
         prioridad: req.body.prioridad,
         incidencia: req.body.incidencia,
