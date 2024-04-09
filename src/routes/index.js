9db042185f83a49c19f22fa973a9dd69b488c622
rin-okumura1 <davidnobati@gmail.com>
2024-04-09 09:32:43 -0300

    ok

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,9 +3,11 @@ const usersRouter = require("./Router/usersRouter")
 const ticketsRouter = require("./Router/ticketsRouter")
 const companyRouter = require("./Router/companyRouter")
 const recordRouter=require('./Router/recordRouter')
-
+const cors=require('cors')
 
 const mainRouter = Router();
+
+mainRouter.use(cors())
 
 mainRouter.use("/usuarios", usersRouter)
 mainRouter.use("/tickets", ticketsRouter)
